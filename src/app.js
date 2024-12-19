55722500c8a154bf6fe735b38900d7c27b916e17
Jainam Jain <92261326+jjain31@users.noreply.github.com>
2024-12-19 23:44:34 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -3,51 +3,42 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cookieParser = require('cookie-parser');
 const cors = require("cors");
-
-// Importing routes
+const app = express();
 const authRouter = require("./routes/auth");
 const profileRouter = require("./routes/profile");
+const seedBanks = require("./Seed/seedBanks");
+const seedUniversity = require("./Seed/seedUniversity");
 const universityBankRouter = require("./routes/universityBankRouter");
-
-// Initialize Express app
-const app = express();
 app.use(cookieParser());
 app.use(express.json());
+
+
 app.use(cors({
-  origin: "http://localhost:5173",
-  credentials: true,
+  origin:"http://localhost:5173",
+  credentials:true,
 }));
 
-// Define routes
-app.use("/auth", authRouter);
+A
+
+app.use("/auth", authRouter);A
 app.use("/profile", profileRouter);
+
 app.use("/student", universityBankRouter);
+
+const MONGODB_URI = process.env.MONGODB_URI;
+const PORT = process.env.PORT || 3000;
 app.get("/", (req, res) => {
     res.send("API is running");
 });
-// MongoDB URI from environment variables
-const MONGODB_URI = process.env.MONGODB_URI;
-
-let isConnected = false; // Track connection status globally
-async function connectToDatabase() {
-  if (!isConnected) {
-    try {
-      await mongoose.connect(MONGODB_URI); // Connect to MongoDB
-      console.log("Database connected successfully");
-      isConnected = true; // Mark as connected
-    } catch (err) {
-      console.error("Database connection error:", err);
-      throw new Error("Database connection failed");
-    }
-  }
-}
-
-module.exports = async (req, res) => {
-  try {
-    await connectToDatabase(); // Ensure DB connection
-    app(req, res); // Delegate request handling to Express
-  } catch (error) {
-    res.status(500).json({ message: "Internal Server Error" });
-  }
-};
-
+mongoose.connect(MONGODB_URI)
+  .then(async() => {
+    console.log("Database connected successfully");
+    await seedBanks();
+    await seedUniversity();
+    app.listen(3000, () => {
+      console.log(`Server started on port 3000`);
+    });
+  })
+  .catch((err) => {
+    console.error("Database connection error:", err);
+  });
