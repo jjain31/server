0afbbc74cfe7da20f46874d39d0f962e2840a8ab
Jainam Jain <92261326+jjain31@users.noreply.github.com>
2024-12-20 00:07:01 +0530

    Update database.js

--- src/database.js
+++ src/database.js
@@ -1,16 +1,26 @@
 // database.js
 const mongoose = require("mongoose");
 
+let isConnected = false; // Track the connection status
+
 async function connectDb() {
   const MONGODB_URI = process.env.MONGODB_URI;
 
+  if (isConnected) {
+    console.log("Using existing database connection");
+    return;
+  }
+
   try {
-    // Mongoose automatically handles useNewUrlParser and useUnifiedTopology
-    await mongoose.connect(MONGODB_URI); 
+    await mongoose.connect(MONGODB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    isConnected = true;
     console.log("Database connected successfully");
   } catch (err) {
     console.error("Database connection error:", err);
-    throw err; // Throw error to be handled in app.js
+    throw err;
   }
 }
 
