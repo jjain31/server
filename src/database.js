2a5be53864a7c0edf412a6f9050a1b478f727eb5
Jainam Jain <92261326+jjain31@users.noreply.github.com>
2024-12-20 00:03:49 +0530

    Update database.js

--- src/database.js
+++ src/database.js
@@ -5,7 +5,8 @@ async function connectDb() {
   const MONGODB_URI = process.env.MONGODB_URI;
 
   try {
-    await mongoose.connect(MONGODB_URI); // No need for `useNewUrlParser` and `useUnifiedTopology` anymore
+    // Mongoose automatically handles useNewUrlParser and useUnifiedTopology
+    await mongoose.connect(MONGODB_URI); 
     console.log("Database connected successfully");
   } catch (err) {
     console.error("Database connection error:", err);
